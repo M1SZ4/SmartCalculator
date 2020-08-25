1d82d47473d03ceb0322d59adf188e9bcc74b064
M1SZ4 <m.wawer@hotmail.com>
2020-08-25 17:20:37 +0200

    Stage 6

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
 
 
-def applyOperation(operator, left, right):
+def apply_operation(operator, left, right):
     answer = left
     if operator == '(':
         return NotImplemented
@@ -24,10 +24,76 @@ def applyOperation(operator, left, right):
 
 
 def calculator(equation):
-    pass
+    # replace variables with numbers
+    letters_indices = [m.span() for m in re.finditer("[a-zA-Z]+", equation)]
+    for letIndex in letters_indices:
+        key = equation[letIndex[0]:letIndex[1]]
+        if key in variables.keys():
+            equation = equation.replace(key, variables[key], 1)
+        else:
+            print("Unknown variable")
+            break
+    # finding initial and last indexes of numbers, saving in tuples
+    else:
+        num_indices = [m.span() for m in re.finditer("\d+", equation)]
+        prev_number = 0
+        prev_end = ''
+
+        for numIndex in num_indices:
+            number = float(equation[numIndex[0]:numIndex[1]])
+
+            # If at the start, just add the number
+            if numIndex[0] == 0:
+                prev_number = number
+                prev_end = numIndex[1]
+                continue
+
+            # Starting at the second spot of the equation is special
+            if numIndex[0] == 1:
+                if equation[0] == "(":
+                    return NotImplemented
+                # It's a negative number
+                elif equation[0] == "-":
+                    prev_number -= number
+                    prev_end = numIndex[1]
+                    continue
+                else:
+                    print("Error. Only numbers, '-' and '(' are allowed at the beginning of the equation.")
+                    exit(1)
+
+            # choosing a characters between numbers
+            operators = ""
+            for o in equation[int(prev_end):].strip():
+                if o in ["+", "-", "*", "/"]:
+                    operators += o
+                else:
+                    break
+            # replace "--" with "+"
+            if "--" in operators and len(operators) % 2 == 0:
+                operators = "+"
+            # if there are several characters, select the first
+            else:
+                operators = operators[0]
+
+            # both numbers are positive
+            if len(operators) == 1:
+                prev_number = apply_operation(operators[0], prev_number, number)
+            # if second number is negative
+            elif len(operators) == 2:
+                if operators[1] == '-':
+                    prev_number = apply_operation(operators[0], prev_number, -number)
+                else:
+                    print("Error. Currently, the second operator must always be a '-'.")
+                    exit(1)
+
+            prev_end = numIndex[1]
+
+        # answer
+        print(int(prev_number))
+
 
 def main():
-    global index, operators
+    global variables
     variables = {}
     while True:
         equation = input()
@@ -45,99 +111,16 @@ def main():
                 print("Unknown command")
 
         elif "=" not in equation:
-            lettersIndices = [m.span() for m in re.finditer("[a-zA-Z]+", equation)]
-            for letIndex in lettersIndices:
-                key = equation[letIndex[0]:letIndex[1]]
-                if key in variables.keys():
-                    equation = equation.replace(key, variables[key], 1)
-                else:
-                    print("Unknown variable")
-                    break
-            else:
-                numIndices = [m.span() for m in re.finditer("\d+", equation)]
-                prevNumber = 0
-                prevEnd = ''
-
-                for numIndex in numIndices:
-                    number = float(equation[numIndex[0]:numIndex[1]])
-
-                    # If at the start, just add the number
-                    if numIndex[0] == 0:
-                        prevNumber = number
-                        prevEnd = numIndex[1]
-                        continue
-
-                    # Starting at the second spot of the equation is special
-                    if numIndex[0] == 1:
-                        # Remember this for order of operations (if implemented)
-                        if equation[0] == "(":
-                            # I think handling these would be best done recursively
-                            # Or you could split on all parentheses and compute each in turn
-                            # Or ...
-                            return NotImplemented
-                        # It's a negative number
-                        elif equation[0] == "-":
-                            prevNumber -= number
-                            prevEnd = numIndex[1]
-                            continue
-                        else:
-                            print("Error. Only numbers, '-' and '(' are allowed at the "
-                                  + "beginning of the equation.")
-                            # You could allow a period as well.
-                            exit(1)
-
-                    # If reached here, then should have passed the first number
-                    # Extract relevant operators and remove any spaces
-                    operators = ""
-                    for o in equation[int(prevEnd):].strip():
-                        if o in ["+", "-"]:
-                            operators += o
-                        else:
-                            break
-
-                    if "--" in operators and len(operators) % 2 == 0:
-                        operators = "+"
-                    else:
-                        operators = operators[0]
-
-                    # operators = equation[prevEnd:numIndex[0]]
-                    # operators = "".join(operators.split())
-
-                    if len(operators) == 1:
-                        prevNumber = applyOperation(operators[0], prevNumber, number)
-
-                    elif len(operators) == 2:
-                        if (operators[1] == '-'):
-                            prevNumber = applyOperation(operators[0], prevNumber, -number)
-                        else:
-                            print("Error. Currently, the second operator must always be a '-'.")
-                            exit(1)
-
-                    # If it reaches here, then parentheses are probably involved
-                    # or syntax is probably incorrect
-                    else:
-                        print("Error. Only two operators are currently allowed between numbers."
-                              + " The second one always being a '-'.")
-                        # You could allow all sorts of nesting with parentheses if you want.
-                        exit(1)
-                    prevEnd = numIndex[1]
-
-                    # Do not display the decimal places if they are all 0
-                prevNumber = int(prevNumber) if prevNumber - int(prevNumber) == 0 else prevNumber
-                # answer
-                print(prevNumber)
+            calculator(equation)
 
         else:
             if equation.count("=") > 1:
                 print("Invalid assignment")
-
             else:
                 try:
                     index = equation.index("=")
-
                 except ValueError:
                     print("Invalid assignment")
-
                 else:
                     key = equation[:index].strip()
                     value = equation[index + 1:].strip()
